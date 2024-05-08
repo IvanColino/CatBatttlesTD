0886f5a561b60867af76a72d9d26f5dcee62b480
DESKTOP-3QQ24VI\ivanc <ivancolino310@gmail.com>
2024-05-08 19:32:17 +0200

    Colocacion de Torres y que sean visible en ambos jugadores

--- Assets/Scripts/BulletBehaviour.cs
+++ Assets/Scripts/BulletBehaviour.cs
@@ -1,15 +1,17 @@
 using UnityEngine;
 using Photon.Pun;
+
 public class BulletBehaviour : MonoBehaviourPun
 {
     private Transform target;
     public float speed = 70f;
-
+    private float originalZ;  // Variable para almacenar la posición z original de la bala
 
     // Método para dirigir la bala hacia el enemigo
     public void Seek(Transform _target)
     {
         target = _target;
+        originalZ = transform.position.z;  // Guarda la posición z original cuando el objetivo es establecido
     }
 
     void Update()
@@ -20,14 +22,15 @@ public class BulletBehaviour : MonoBehaviourPun
             return;
         }
 
-        Vector2 direction = (target.position - transform.position).normalized;
+        Vector2 direction = (new Vector2(target.position.x, target.position.y) - new Vector2(transform.position.x, transform.position.y)).normalized;
         float distanceThisFrame = speed * Time.deltaTime;
 
         // Mover la bala hacia el objetivo
-        transform.position = Vector2.MoveTowards(transform.position, target.position, distanceThisFrame);
+        Vector3 newTargetPos = new Vector3(target.position.x, target.position.y, originalZ); 
+        transform.position = Vector3.MoveTowards(transform.position, newTargetPos, distanceThisFrame);
 
         // Verificar si la bala ha alcanzado al objetivo
-        if (Vector2.Distance(transform.position, target.position) < 0.1f)
+        if (Vector3.Distance(transform.position, newTargetPos) < 0.1f)
         {
             HitTarget();
         }
@@ -36,10 +39,7 @@ public class BulletBehaviour : MonoBehaviourPun
     [PunRPC]
     void HitTarget()
     {
-       
-
-       
-        Destroy(target.gameObject); // Destruir el objeto del enemigo
-        Destroy(gameObject); // Destruir la bala
+         Destroy(target.gameObject); // Destruir el objeto del enemigo (opcional)
+         Destroy(gameObject); // Destruir la bala
     }
 }
