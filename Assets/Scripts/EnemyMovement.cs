5851deddaa1a553fe015568e0cb40774eb2d3d4b
DESKTOP-3QQ24VI\ivanc <ivancolino310@gmail.com>
2024-05-13 17:48:08 +0200

    Dar puntos al jugador que ha ganado

--- Assets/Scripts/EnemyMovement.cs
+++ Assets/Scripts/EnemyMovement.cs
@@ -1,6 +1,9 @@
 using Photon.Pun;
+using System.Collections;
 using TMPro;
 using UnityEngine;
+using UnityEngine.Networking;
+using static System.Net.WebRequestMethods;
 
 public class MoveEnemies : MonoBehaviourPun
 {
@@ -10,6 +13,8 @@ public class MoveEnemies : MonoBehaviourPun
     private GameObject[] waypointsObjects = new GameObject[12];
     public int playerHealth = 0;
     private TextMeshProUGUI text;
+    private string url = "https://catbattle.duckdns.org/api/win";
+
 
     void Start()
     {
@@ -71,14 +76,44 @@ public class MoveEnemies : MonoBehaviourPun
     [PunRPC]
     void DamagePlayer()
     {
-        playerHealth =playerHealth- 1;  
-        Debug.Log("Player Health: " + playerHealth);
-        text.text = int.Parse(text.text)-1 + "";
-        if (playerHealth <= 0)
+
+        playerHealth -= 10;
+        text.text = playerHealth.ToString(); // Actualiza la UI solo aquí
+
+        if (PhotonNetwork.IsMasterClient && playerHealth <= 0)
         {
-            //muerte del jugador
-            Debug.Log("Player died!");
+            StartCoroutine(SendRequest());
         }
     }
-}
+
+
+
+               
+              
+            
+        
     
+
+    IEnumerator SendRequest()
+    {
+        string url = "https://catbattle.duckdns.org/api/win";
+        string json = "{\"user_id\": 7}"; // Formato correcto de JSON como string
+        UnityWebRequest request = UnityWebRequest.Put(url, json); // Usar PUT si la API lo requiere, o cambiar a POST si es necesario
+        Debug.Log("Enviando solicitud a " + url + " con JSON: " + json);
+        request.SetRequestHeader("Content-Type", "application/json"); // Establecer el encabezado Content-Type como application/json
+        request.method = "POST"; // Asegúrate de utilizar el método HTTP correcto que la API espera (POST, en este caso)
+
+        yield return request.SendWebRequest(); // Enviar la solicitud y esperar a que se complete
+
+        if (request.result == UnityWebRequest.Result.ConnectionError || request.result == UnityWebRequest.Result.ProtocolError)
+        {
+            Debug.LogError("Error al enviar la solicitud: " + request.error);
+        }
+        else
+        {
+            Debug.Log("Respuesta de la solicitud: " + request.downloadHandler.text);
+        }
+    }
+
+}
+
